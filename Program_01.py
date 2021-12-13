4c01e2c53868ece27d901b7a1777524dfbf38b1f
JustinVillas <92616184+JustinVillas@users.noreply.github.com>
2021-12-13 17:25:53 +0800

    I've used the math function to convert decimal number

--- Program_01.py
+++ Program_01.py
@@ -4,47 +4,41 @@
 # Input grade: 87.6
 # Equivalent Grade/Mark: 1.75
 # Description: Very Good
+import math
 
 # Steps
 # 1. Ask student their grade percentage.
-student_grade = float(input("Grade: "))
-
+grade_int = float(input("Grade: "))
+student_grade = math.ceil(grade_int)
 if student_grade >= 97 and student_grade <= 100:
-    print("""Equivalent: 1.0
-Description: Excellent""")
+    print("Equivalent: 1.0 ")
+    print("Description: Excellent")
+elif student_grade >= 94 and student_grade <= 96:
+    print("Equivalent: 1.25")
+    print("Description: Excellent")
+elif student_grade >= 91 and student_grade <= 93:
+    print("Equivalent: 1.5")
+    print("Description: Very Good")
+elif student_grade >= 88 and student_grade <= 90:
+    print("Equivalent: 1.75")
+    print("Description: Very Good")
+elif student_grade >= 85 and student_grade <= 87:
+    print("Equivalent: 2.0")
+    print("Description: Good")
+elif student_grade >= 82 and student_grade <= 84:
+    print("Equivalent: 2.25")
+    print("Description: Good")
+elif student_grade >= 79 and student_grade <= 81:
+    print("Equivalent: 2.5")
+    print("Description: Satisfactory")
+elif student_grade >= 76 and student_grade <= 78:
+    print("Equivalent: 2.75")
+    print("Description: Satisfactory")
+elif student_grade == 75:
+    print("Equivalent: 3.0")
+    print("Description: Passing")
+elif student_grade > 65 and student_grade <= 74:
+    print("Grade/Mark: 5.0")
+    print("Description: Failure")
 else:
-    if student_grade >= 94 and student_grade <= 96:
-        print("""Equivalent: 1.25
-Description: Excellent""")
-    else:
-        if student_grade >= 91 and student_grade <= 93:
-            print("""Equivalent: 1.5
-Description: Very Good""")
-        else:
-            if student_grade >= 88 and student_grade <= 90:
-                print("""Equivalent: 1.75
-Description: Very Good""")
-            else:
-                if student_grade >= 85 and student_grade <= 87:
-                    print("""Equivalent: 2.0
-Description: Good""")
-                else:
-                    if student_grade >= 82 and student_grade <= 84:
-                        print("""Equivalent: 2.25
-Description: Good""")
-                    else:
-                        if student_grade >= 79 and student_grade <= 81:
-                            print("""Equivalent: 2.5
-Description: Satisfactory""")
-                        else:
-                            if student_grade >= 76 and student_grade <= 78:
-                                print("""Equivalent: 2.75
-Description: Satisfactory""")
-                            else:
-                                if student_grade == 75:
-                                    print("""Equivalent: 3
-Description: Passing""")
-                                else:
-                                    if student_grade >= 65 and student_grade <= 74:
-                                        print("""Equivalent: 5.0
-Description: Failure""")
+    print("Sorry, you've inputed a wrong data. Thank you ")
